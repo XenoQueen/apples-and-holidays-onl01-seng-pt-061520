1d9d48b69cd48b91a16a371e9004b499725d3824
Maika Kailani <queenmab051@gmail.com>
2020-06-30 02:44:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
     data.each do |holiday, supply|
       array = holiday.to_s.split("_")
       final_holiday = []
+      
 end
 
 def all_holidays_with_bbq(holiday_hash)
