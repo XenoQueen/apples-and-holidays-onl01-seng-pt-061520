750991e408142c528913d0a0f8fbf03efbd0a47a
Maika Kailani <queenmab051@gmail.com>
2020-06-30 02:46:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
         end
       holiday = final_holiday.join(" ")
       supply = supply.join(" , ")
+      puts 
 end
 
 def all_holidays_with_bbq(holiday_hash)
