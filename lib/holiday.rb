5416bb03bda6092e1e723e37668bcf1baac7093a
Maika Kailani <queenmab051@gmail.com>
2020-06-30 02:43:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize!}:"
     data.each do |holiday, supply|
-      array = holiday.to_s
+      array = holiday.to_s.split
 end
 
 def all_holidays_with_bbq(holiday_hash)
